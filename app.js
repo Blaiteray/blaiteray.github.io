ff67f67759b48fe1a75a4624ed10bf1d992c081f
Blaiteray <66131047+Blaiteray@users.noreply.github.com>
2021-02-08 09:33:07 +0600

    adding dropdown

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-const button3 = document.querySelector("button3");
-const projectList = document.querySelector("project-list");
-button3.addEventListener("click", toggleDropdownVisibility());
-const toggleDropdownVisibility = () => {
-  projectList.style.display = "grid";
-};
+var button3 = document.querySelector("#button3");
+var projectList = document.querySelector(".project-list");
+button3.addEventListener("click", toggleDropdownVisibility);
+
+function toggleDropdownVisibility() {
+  projectList.style.display =
+    projectList.style.display != "none" ? "none" : "grid";
+}
+
+window.addEventListener("click", closeDropdown);
+function closeDropdown(event) {
+  if (!event.target.matches("#button3")) {
+    projectList.style.display = "none";
+  }
+}
