13cd42ea187180f58d31d0fc63c425249a62751a
Blaiteray <66131047+Blaiteray@users.noreply.github.com>
2021-04-01 01:37:45 +0600

    combine success

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+let i;
 const button3 = document.querySelector("#button3");
 const projectList = document.querySelector(".project-list");
 button3.addEventListener("click", toggleDropdownVisibility);
@@ -14,17 +15,34 @@ function closeDropdown(event) {
   }
 }
 
+const homeMid = document.getElementById("homeMid");
+const contMid = document.getElementById("contMid");
+
+const dispValue = ["-webkit-flex", "flex"];
+
 const logoName = document.getElementById("logo-name");
 logoName.onclick = function () {
-  location.href = "./index.html";
+  for (i = 0; i < 2; i++) {
+    homeMid.style.display = dispValue[i];
+    if (window.getComputedStyle(logoName).display === dispValue[i]) break;
+  }
+  contMid.style.display = "none";
 };
 
 const button1 = document.getElementById("button1");
 button1.onclick = function () {
-  location.href = "./index.html";
+  for (i = 0; i < 2; i++) {
+    homeMid.style.display = dispValue[i];
+    if (window.getComputedStyle(logoName).display === dispValue[i]) break;
+  }
+  contMid.style.display = "none";
 };
 
 const button2 = document.getElementById("button2");
 button2.onclick = function () {
-  location.href = "./contact.html";
+  homeMid.style.display = "none";
+  for (i = 0; i < 2; i++) {
+    contMid.style.display = dispValue[i];
+    if (window.getComputedStyle(logoName).display === dispValue[i]) break;
+  }
 };
