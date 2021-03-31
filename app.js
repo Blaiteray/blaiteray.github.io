b012489041f11ed338b376f8fd30294ad09af0eb
Blaiteray <66131047+Blaiteray@users.noreply.github.com>
2021-04-01 00:03:02 +0600

    modifying contact page and background

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var button3 = document.querySelector("#button3");
-var projectList = document.querySelector(".project-list");
+const button3 = document.querySelector("#button3");
+const projectList = document.querySelector(".project-list");
 button3.addEventListener("click", toggleDropdownVisibility);
 
 function toggleDropdownVisibility() {
@@ -13,3 +13,18 @@ function closeDropdown(event) {
     projectList.style.display = "none";
   }
 }
+
+const logoName = document.getElementById("logo-name");
+logoName.onclick = function () {
+  location.href = "./index.html";
+};
+
+const button1 = document.getElementById("button1");
+button1.onclick = function () {
+  location.href = "./index.html";
+};
+
+const button2 = document.getElementById("button2");
+button2.onclick = function () {
+  location.href = "./contact.html";
+};
